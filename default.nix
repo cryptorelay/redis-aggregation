21baf30451f0f5e0a9ad4a9321ca79d8fff4f033
yihuang <yi.codeplayer@gmail.com>
2020-10-03 19:53:18 +0800

    fix nix build on linux

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ system ? builtins.currentSystem, pkgs ? import <nixpkgs> { inherit system; } }:
 with pkgs;
 rustPlatform.buildRustPackage {
   pname = "redis-aggregation";
@@ -15,4 +15,9 @@ rustPlatform.buildRustPackage {
     ];
   };
   cargoSha256 = sha256:1rcxkhazhfbswqq99n9a68xvhl7ywk1n3gqi412bv89ikyl3jfzr;
+  buildInputs = [llvmPackages.libclang.lib clangStdenv.cc.libc];
+  LIBCLANG_PATH="${llvmPackages.libclang.lib}/lib";
+  preConfigure = ''
+    export BINDGEN_EXTRA_CLANG_ARGS="-isystem ${clang}/resource-root/include $NIX_CFLAGS_COMPILE"
+  '';
 }
