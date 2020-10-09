8c74b42561ade27b42ed966c2e6e70b38fbcedb1
yihuang <yi.codeplayer@gmail.com>
2020-10-09 18:27:40 +0800

    github action to push docker image

---
+++ docker.nix
@@ -0,0 +1,14 @@
+{ system ? "x86_64-linux", pkgs ? import <nixpkgs> { inherit system; } }:
+with pkgs;
+let
+  module = import ./. { inherit pkgs; };
+in
+dockerTools.buildLayeredImage {
+  name = "cryptorelay/redis-aggregation";
+  tag = "latest";
+  config.Entrypoint = [
+    "${redis}/bin/redis-server"
+    "--loadmodule"
+    "${module}/lib/libredis_aggregation.so"
+  ];
+}
